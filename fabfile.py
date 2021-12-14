4f0e69c3dbfda72c59b4a31a8f8c98ebe575f1d5
Ramananda Kairi <ramananda.kairi@gmail.com>
2021-12-14 14:27:27 +0530

    Dockerized for deployment

---
+++ fabfile.py
@@ -0,0 +1,11 @@
+from fabric.api import cd, env, local, prefix, run
+
+env.hosts = ['ubuntu@159.100.241.165']
+env.BASE_DIR = "blog-api"
+
+
+def deploy():
+    with cd(env.BASE_DIR):
+        run("git pull")
+        run("docker-compose -f docker-compose.yml up -d --build")
+        run("docker exec -it webapp alembic upgrade head")
