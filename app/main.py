ed35088ef8ea7c9a656575414df1a4e051562956
Ramananda Kairi <ramananda.kairi@gmail.ccom>
2022-01-16 21:39:40 +0530

    cors fixed -1

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost:3000", "localhost:8000", "localhost:8080"],
+    allow_origins=["http://localhost:3000", "http://localhost:8000", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
